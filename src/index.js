c914bdd27e36b082c6be162ce800bb0d78fc73d0
unknown <kevinjhampier5@gmail.com>
2025-08-12 15:41:48 -0500

    first commit - web server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* function expression */
+import app from "./app.js";
+const main = () =>{
+    app.listen(app.get("port"))
+    console.log(`The company's server web is running on port ${app.get("port")}`);
+    
+}
+
+main ();
+
